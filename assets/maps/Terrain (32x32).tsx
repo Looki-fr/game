5989ec604408a23313abd11fd872c2a28a2f0efe
Looki-fr <74359642+Looki-fr@users.noreply.github.com>
2021-09-21 17:55:17 +0100

    Add files via upload

---
+++ assets/maps/Terrain (32x32).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="Terrain (32x32)" tilewidth="16" tileheight="16" tilecount="988" columns="38">
+ <image source="Sprites/14-TileSets/Terrain (32x32).png" trans="000000" width="608" height="416"/>
+</tileset>
