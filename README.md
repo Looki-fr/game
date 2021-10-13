1cc37d4ec292cfab3edfdff2439e9aa96258e059
Looki-fr <lemeilllouis@gmail.com>
2021-10-13 21:22:53 +0100

    new file:   README.md 	new file:   __pycache__/dash_images.cpython-39.pyc 	new file:   __pycache__/game.cpython-39.pyc 	new file:   __pycache__/particule.cpython-39.pyc 	new file:   __pycache__/player.cpython-39.pyc 	new file:   __pycache__/textSprite.cpython-39.pyc 	new file:   __pycache__/tileMap.cpython-39.pyc 	new file:   __pycache__/toucheSprite.cpython-39.pyc     new file:   entities_sprite/__pycache__/dash_images.cpython-39.pyc 	new file:   entities_sprite/__pycache__/particule.cpython-39.pyc 	new file:   entities_sprite/dash_images.py 	new file:   entities_sprite/particule.py 	new file:   map_sprite/__pycache__/textSprite.cpython-39.pyc 	new file:   map_sprite/__pycache__/toucheSprite.cpython-39.pyc 	new file:   map_sprite/textSprite.py 	new file:   map_sprite/toucheSprite.py

---
+++ README.md
@@ -0,0 +1,2 @@
+# game
+A little video game (in its early days) in python, should be updated every week. Feel free to give me feedbacks --> Discord : Looki#0302 | mail : blablalooki@gmail.com
---
+++ map_sprite/textSprite.py
@@ -0,0 +1,17 @@
+import pygame
+import time
+from pygame.locals import *
+
+class TextSprite():
+    """ class affichant une image du joueur quand il dash pour une courte duree"""
+    def __init__(self, x, y ,id, directory):
+        self.id = id
+        self.image = pygame.image.load(f'{directory}\\assets\\text\\{id}.png')
+        self.image = pygame.transform.scale(self.image, (round(self.image.get_width()*0.6), round(self.image.get_height()*0.6)))
+        self.image.set_colorkey(self.image.get_at((0,0)))
+        self.x = x
+        self.y = y
+        self.rect = self.image.get_rect()
+        
+    def update(self):
+        self.rect.topleft = (self.x, self.y)
